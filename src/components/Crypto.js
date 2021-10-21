aa20970359831cd9ae04ea7adf2b1148938f003b
Chris Boydstun <chrisboydstun@gmail.com>
2021-10-21 16:20:58 -0500

    centered text

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -1,60 +1,59 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import Button from 'react-bootstrap/Button'
-import { Spinner } from 'react-bootstrap'
+import Button from "react-bootstrap/Button";
+import { Spinner } from "react-bootstrap";
 
-const baseUrl = 'https://api.coincap.io/v2/assets/bitcoin'
+const baseUrl = "https://api.coincap.io/v2/assets/bitcoin";
 
 export default function Bitcoin() {
-  const [value, setValue] = useState({})
+  const [value, setValue] = useState({});
 
-  let dollarUSA = Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  })
+  let dollarUSA = Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
 
-  //make API call to get bitcoin data
   useEffect(() => {
     axios
       .get(baseUrl, {
         headers: {
-          'content-type': 'application/json',
+          "content-type": "application/json",
         },
       })
       .then((res) => {
-        setValue(res.data)
-        console.log(res.data)
+        setValue(res.data);
+        console.log(res.data);
       })
       .catch((err) => {
-        console.log(err)
-      })
-  }, [])
+        console.log(err);
+      });
+  }, []);
 
   const refreshPrice = () => {
     axios
       .get(baseUrl, {
         headers: {
-          'content-type': 'application/json',
+          "content-type": "application/json",
         },
       })
       .then((res) => {
-        setValue(res.data)
-        console.log(res.data)
+        setValue(res.data);
+        console.log(res.data);
       })
       .catch((err) => {
-        console.log(err)
-      })
-  }
+        console.log(err);
+      });
+  };
 
   return (
     <div className="card">
       {value.data ? (
-        <div>
+        <div className="text-center">
           <h1>Name: {value.data.name}</h1>
           <h2>Price: {dollarUSA.format(value.data.priceUsd)}</h2>
           <h3>Symbol: {value.data.symbol}</h3>
-          <Button onClick={refreshPrice}>Button</Button>
+          <Button onClick={refreshPrice}>Refresh</Button>
         </div>
       ) : (
         <Spinner animation="border" role="status">
@@ -62,5 +61,5 @@ export default function Bitcoin() {
         </Spinner>
       )}
     </div>
-  )
+  );
 }
